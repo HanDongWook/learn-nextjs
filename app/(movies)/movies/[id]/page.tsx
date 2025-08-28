1d0c5fbd1d8788cecfbde71d899b5ea41057bded
HanDongWook <jja9312345@gmail.com>
2025-08-28 16:56:29 +0900

    Align PageProps with Next.js requirements.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,25 +2,26 @@ import { Suspense } from "react";
 import MovieInfo, { fetchMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
+type PageProps = {
     params: { id: string };
-}
+    searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export async function generateMetadata({ params }: IParams) {
-    const movie = await fetchMovie(params.id);
+export async function generateMetadata({ params: { id } }: PageProps) {
+    const movie = await fetchMovie(id);
     return {
         title: movie.title,
     }
 }
 
-export default async function MovieDetail({ params }: IParams) {
+export default async function MovieDetail({ params: { id } }: PageProps) {
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
-                <MovieInfo id={params.id} />
+                <MovieInfo id={id} />
             </Suspense>
             <Suspense fallback={<h1>Loading movie videos</h1>}>
-                <MovieVideos id={params.id} />
+                <MovieVideos id={id} />
             </Suspense>
         </div>
     );
