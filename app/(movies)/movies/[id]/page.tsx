cf18c76d992427b0970be778c4a7b9691298380c
HanDongWook <jja9312345@gmail.com>
2025-08-28 16:53:05 +0900

    fix generateMetadata

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,24 +3,24 @@ import MovieInfo, { fetchMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
 interface IParams {
-    params: {id: string};
+    params: { id: string };
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
-    const movie = await fetchMovie(id);
+export async function generateMetadata({ params }: IParams) {
+    const movie = await fetchMovie(params.id);
     return {
         title: movie.title,
     }
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetail({ params }: IParams) {
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
-                <MovieInfo id={id} />
+                <MovieInfo id={params.id} />
             </Suspense>
             <Suspense fallback={<h1>Loading movie videos</h1>}>
-                <MovieVideos id={id} />
+                <MovieVideos id={params.id} />
             </Suspense>
         </div>
     );
