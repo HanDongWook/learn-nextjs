3cbc14ca7ba4ac4dec67655325d75347e67f6117
HanDongWook <jja9312345@gmail.com>
2025-08-28 16:58:50 +0900

    Align PageProps with Next.js requirements(2).

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,17 +1,18 @@
 import { Suspense } from "react";
 import MovieInfo, { fetchMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+import { Metadata } from "next";
 
 type PageProps = {
     params: { id: string };
     searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata({ params: { id } }: PageProps) {
-    const movie = await fetchMovie(id);
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+    const movie = await fetchMovie(params.id);
     return {
         title: movie.title,
-    }
+    };
 }
 
 export default async function MovieDetail({ params: { id } }: PageProps) {
